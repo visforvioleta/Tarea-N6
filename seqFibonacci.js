8f24848d19d20387c9a18280b8c5a7639aba6c9d
Violeta Alanoca <v.isforvioleta@gmail.com>
2024-11-25 23:39:57 -0300

    Filtrado de Pares e Impares + Comentarios

--- seqFibonacci.js
+++ seqFibonacci.js
@@ -3,8 +3,10 @@
 // Números pares entre 0 y 1000.
 // Números impares entre 0 y 1000.
 
-let fib = [0, 1]
 
+//Inicio secuencia Fibonacci
+let fib = [0, 1]
+//Función que irá añadiendo nuevos números a la secuencia hasta los 1000
 function fibonacciHastaMil() {
     let numeroActual;
     for (let indice = 2; true; indice++) {
@@ -18,5 +20,24 @@ function fibonacciHastaMil() {
     return fib;
 }
 
+//Función que recibe un arreglo y retorna el mismo arreglo filtrando sólo los números par
+function filtrarPares(arreglo){
+    return arreglo.filter((valor)=>{
+        return valor%2 === 0;
+    })
+}
+
+//Función que recibe un arreglo y retorna el mismo arreglo filtrando sólo los números impar
+function filtrarImpares(arreglo){
+    return arreglo.filter((valor)=>{
+        return valor%2 === 1;
+    })
+}
+
 const arregloDeFibonacciHastaMil = fibonacciHastaMil();
+
 console.log("Los números de fibonacci hasta el número 1000: " + arregloDeFibonacciHastaMil)
+
+console.log("Los número pares dentro de la secuencia fibonacci son: " + filtrarPares(arregloDeFibonacciHastaMil))
+
+console.log("Los número impares dentro de la secuencia fibonacci son: " + filtrarImpares(arregloDeFibonacciHastaMil))
