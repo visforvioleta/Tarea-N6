31a3b58a4c4e30173e3743756b4ee206fd783cc2
Violeta Alanoca <v.isforvioleta@gmail.com>
2024-11-25 23:29:43 -0300

    Secuencia Fibonacci Hasta Mil

--- seqFibonacci.js
+++ seqFibonacci.js
@@ -3,4 +3,20 @@
 // Números pares entre 0 y 1000.
 // Números impares entre 0 y 1000.
 
-let fibonacci = [0, 1]
+let fib = [0, 1]
+
+function fibonacciHastaMil() {
+    let numeroActual;
+    for (let indice = 2; true; indice++) {
+        numeroActual = fib[indice - 1] + fib[indice - 2]
+        if (numeroActual < 1000) {
+            fib.push(numeroActual);
+        } else {
+            break;
+        }
+    }
+    return fib;
+}
+
+const arregloDeFibonacciHastaMil = fibonacciHastaMil();
+console.log("Los números de fibonacci hasta el número 1000: " + arregloDeFibonacciHastaMil)
